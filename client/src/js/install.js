74c94e02dc1ba7a245477ad2ee974bbf8184bb45
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-09-16 01:18:16 -0700

    fixed putDB

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,29 +5,27 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     // Stash the event so it can be triggered later.
     window.deferredPrompt = event;
-    // Update the install button's display
-    butInstall.style.display = 'block';
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    // Hide our user interface that shows the "Install" button
-   butInstall.style.display = 'none';
-
-  // Show the install prompt
+    // Show the install prompt
     const promptEvent = window.deferredPrompt;
-if (!promptEvent) {
-    return;
-}
- // Show prompt
-promptEvent.prompt();
-// Reset the deferred prompt variable, it can only be used once.
-window.deferredPrompt = null;
-butInstall.classList.toggle('hidden', true);
+    if (!promptEvent) {
+        return;
+    }
+    // Show prompt
+    promptEvent.prompt();
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // clear prompt
-    window.deferredPrompt = null;   
+    window.deferredPrompt = null;
 });
